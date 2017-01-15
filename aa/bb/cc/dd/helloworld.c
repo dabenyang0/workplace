85d047cfe147e42ed9b4d2beed7d5bce4a26e4ba
yangyang <yangyang@example.com>
2017-01-15 13:23:00 +0800

    wwwww

--- aa/bb/cc/dd/helloworld.c
+++ aa/bb/cc/dd/helloworld.c
@@ -6,6 +6,10 @@
 int main(int argc, const char *argv[])
 {
 	printk("hello world\n");
+	printk("hello world\n");
+	printk("hello world\n");
+	printk("hello world\n");
+	printk("hello world\n");
 	return 0;
 }
 
