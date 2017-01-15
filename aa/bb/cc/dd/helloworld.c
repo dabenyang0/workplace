8a48f26ce313d0ade2d7170f66bc99dd55bb18d6
yangyang <yangyang@example.com>
2017-01-15 13:29:18 +0800

    add contxt yangyang

--- aa/bb/cc/dd/helloworld.c
+++ aa/bb/cc/dd/helloworld.c
@@ -10,6 +10,12 @@ int main(int argc, const char *argv[])
 	printk("hello world\n");
 	printk("hello world\n");
 	printk("hello world\n");
+
+	printf("yangyang\n");
+	printf("yangyang\n");
+	printf("yangyang\n");
+	printf("yangyang\n");
+	printf("yangyang\n");
 	return 0;
 }
 
